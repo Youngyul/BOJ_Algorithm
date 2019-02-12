6d1c0b944431e0b8099e3863767a9b4b3330e49b
Youngyul <31728373+Youngyul@users.noreply.github.com>
2019-02-12 17:03:18 +0900

    Update BOJ_3055.java

--- BOJ_Algorithm/src/BOJ_3055.java
+++ BOJ_Algorithm/src/BOJ_3055.java
@@ -1,231 +1,201 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/**
+ * BOJ#3055 탈출
+ * https://www.acmicpc.net/problem/3055
+ */
 
 public class BOJ_3055 {
 
-	static int R,C;
-	static String arr [][];
-	static boolean water [][];
-	static boolean check [][];
-	static int cnt;
-	static int xx [] = {-1,1,0,0};
-	static int yy [] = {0,0,-1,1};
-	static int watercnt [][];
-	static int bfscnt[][];
-	static int x,y;
-	static int watercount;
-	
-	//S에서 D로가는 bfs;
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		R = sc.nextInt();
-		C = sc.nextInt();
-		
-		arr = new String [R][C];
-		
-		for(int i = 0 ; i<R;i++) {
-			String str = sc.next();
-		for(int j  = 0 ;j<C;j++) {
-			arr[i][j]= str.substring(j, j+1);
-		}
-		}
-		//비버굴 D,고슴도치 S, 돌X,물 *, 인접한 4칸이동. 물도 4칸 확장.
-		//고슴도치 CAN'T GO WATER, WATER CAN'T GO BIBER
-		
-		water = new boolean[R][C];
-		check = new boolean[R][C];
-		cnt = 0;
-		watercnt = new int[R][C];
-		bfscnt = new int[R][C];
-		
-		for(int k = 0  ; k<R;k++) {
-			for(int p = 0 ; p<C;p++) {
-				if(arr[k][p].equals("*")) {
-				
-					watercnt[k][p]=0;
-					water = new boolean[R][C];
-					
-					//물이 시작하는 곳이 두군데 이상인 경우. 결과값이 바뀌어야한다.
-					
-					BFS_WATER(k,p);	
-				}
-			}
-		}
-	
-		
-		//WATERCNT를 확인하기 위한 코드
-		for(int k = 0  ; k<R;k++) {
-			for(int p = 0 ; p<C;p++) {
-			System.out.print(watercnt[k][p]);
-			}
-			System.out.println();
-		}
-		
-		
-		
-		
-		
-		for(int k = 0  ; k<R;k++) {
-			for(int p = 0 ; p<C;p++) {
-				if(arr[k][p].equals("S")) {
-					BFS(k,p);	
-				}
-			}
-		}
-		
-		
-		//BFSCNT를 확인하기 위한 코드
-		System.out.println();
-		
-		for(int k = 0  ; k<R;k++) {
-			for(int p = 0 ; p<C;p++) {
-			System.out.print(bfscnt[k][p]);
-			}
-			System.out.println();
-		}
-		
-		
-		//결과 표출.
-		for(int i = 0 ; i<R;i++) {
-			for(int j = 0 ;j<C;j++) {
-				if(arr[i][j].equals("D")) {
-				//목적지 상하좌우.
-					if(bfscnt[i][j]!=0) {
-						System.out.println(bfscnt[i][j]);
-					}
-					else {
-						System.out.println("KAKTUS");
-					}
-				}
-			}
-		}
-		
-	
-		
-	}
-	
-	//BFS_WATER 완벽.
-	static void BFS_WATER(int a,int b) {
-		
-		watercnt[a][b]=0;
+    static final int BLANK = 0;
+    static final int WATER = -1;
+    static final int STONE = -2;
+    static final int CAVE = 1;
 
-		
-		Queue<Pair> qwater = new LinkedList<>();
-		qwater.add(new Pair(a,b));
-		water[a][b]=true;
-		while(!qwater.isEmpty()) {
-			Pair nowwater = qwater.remove();
-			int nowwaterx = nowwater.x;
-			int nowwatery = nowwater.y;
-			for(int i = 0 ; i<4;i++) {
-				int nextwaterx = nowwaterx+xx[i];
-				int nextwatery = nowwatery+yy[i];
-				if(nextwaterx>=0&&nextwaterx<R&&nextwatery>=0&&nextwatery<C) {
-					if(water[nextwaterx][nextwatery]==false&&arr[nextwaterx][nextwatery].equals(".")) {
-						if(watercnt[nextwaterx][nextwatery]==0) {
-						watercnt[nextwaterx][nextwatery]=watercnt[nowwaterx][nowwatery]+1;
-						water[nextwaterx][nextwatery]=true;
-						qwater.add(new Pair(nextwaterx,nextwatery));
-						}
-						else {
-							int watercount = watercnt[nextwaterx][nextwatery];
-							if(watercount>watercnt[nowwaterx][nowwatery]+1) {
-								watercnt[nextwaterx][nextwatery]=watercnt[nowwaterx][nowwatery]+1;
-								water[nextwaterx][nextwatery]=true;
-								qwater.add(new Pair(nextwaterx,nextwatery));
-							}
-							else {
-								watercnt[nextwaterx][nextwatery]=watercount;
-								water[nextwaterx][nextwatery]=true;
-								qwater.add(new Pair(nextwaterx,nextwatery));
-							}
-						}
-					}
-					if(water[nextwaterx][nextwatery]==false&&arr[nextwaterx][nextwatery].equals("D")) {
-						if(watercnt[nextwaterx][nextwatery]==0) {
-							watercnt[nextwaterx][nextwatery]=watercnt[nowwaterx][nowwatery]+1;
-						}
-						else {
-							int nowwatercnt = watercnt[nextwaterx][nextwatery];
-							if(nowwatercnt<watercnt[nowwaterx][nowwatery]+1) {
-								watercnt[nextwaterx][nextwatery]=watercnt[nowwaterx][nowwatery]+1;
-							}
-						}
-					}
-				}
-			}
-		}
-		
-		
-	}
-	
-	
-	
-	static void BFS(int a, int b) {
-		
-		//(a,b)는 시작위치 D, (c,d)는 물의 위치.
-		Queue<Pair> q = new LinkedList<>();
-	
-		
-		q.add(new Pair(a,b));
-		check[a][b]=true;
-		bfscnt[a][b]=0;
-		
-		
-		while(!q.isEmpty()) {
-			Pair nowp = q.remove();
-			int nowx = nowp.x;
-			int nowy = nowp.y;
-			
-		
-		
-			for(int i = 0 ;i<4;i++) {
-				int nextx = nowx+xx[i];
-				int nexty = nowy+yy[i];
-				
-				if(nextx>=0&&nextx<R&&nexty>=0&&nexty<C) {
-					if(check[nextx][nexty]==false) {
-						//방문한적이 없으며, 다음위치가 구간안에 있을경우. 평소라면 q에 넣는다. 
-						// 이문제의 경우, 방문한적없으며, 이동이 가능한, .과 s인 경우인경우를 추가해야하고.
-						if(arr[nextx][nexty].equals(".")||arr[nextx][nexty].equals("D")) {
-							//.과 s임에도 불구하고, 만약 
-							
-								//이동가능.
-							    if(bfscnt[nowx][nowy]<watercnt[nextx][nexty]-1) {
-								check[nextx][nexty]=true;
-								bfscnt[nextx][nexty]=bfscnt[nowx][nowy]+1;
-								q.add(new Pair(nextx,nexty));
-								}
-							   
-						}
-				
-					}
-				}
-				
-				
-			}
-		}
-		
-	
-		}
-		
-	
-	
-	static class Pair{
-		int x;
-		int y;
-	
-		Pair(int x, int y) {
-			this.x=x;
-			this.y=y;
-			
-		}
-	}
-	
-	
-	}
-	
-	
+    static final int[] dRow = {0, -1, 0, 1};
+    static final int[] dCol = {-1, 0, 1, 0};
+
+    static int[][][] map = new int[51][51][2000];
+    static boolean[][] waterDiscovered = new boolean[51][51];
+    static boolean[][] hogDiscovered = new boolean[51][51];
+
+    public static void main(String[] args) throws IOException {
+
+        int R, C;
+        ArrayList<Point> waterPos = new ArrayList<Point>();
+        Point hogPos = null;
+
+        // 1. input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < R; i++) {
+
+            String lineInfo = br.readLine();
+            for (int j = 0; j < C; j++) {
+
+                char c = lineInfo.charAt(j);
+
+                switch (c) {
+
+                    case '.': // 빈칸
+                        map[i][j][0] = BLANK;
+                        break;
+
+                    case '*': // 물
+                        map[i][j][0] = WATER;
+                        waterPos.add(new Point(i, j));
+                        break;
+
+                    case 'X': // 돌
+                        map[i][j][0] = STONE;
+                        break;
+
+                    case 'D': // 동굴
+                        map[i][j][0] = CAVE;
+                        break;
+
+                    case 'S': // 고슴도치 위치
+                        map[i][j][0] = BLANK;
+                        hogPos = new Point(i, j);
+                        break;
+                }
+            }
+        }
+
+        // 2. water
+        Queue<Point> waterQueue = new LinkedList<Point>();
+
+        for (Point x : waterPos) {
+
+            waterQueue.add(x);
+            waterDiscovered[x.row][x.col] = true;
+        }
+
+        int step = 0;
+        while (!waterQueue.isEmpty()) {
+
+            step++;
+
+            if (step > 0) {
+
+                for (int x = 0; x < R; x++) {
+
+                    for (int y = 0; y < C; y++) {
+
+                        map[x][y][step] = map[x][y][step - 1];
+                    }
+                }
+            }
+
+            int size = waterQueue.size();
+            for (int i = 0; i < size; i++) {
+
+                Point u = waterQueue.poll();
+
+                // 상, 하, 좌, 우
+                for (int j = 0; j < 4; j++) {
+
+                    int nextRow = u.row + dRow[j];
+                    int nextCol = u.col + dCol[j];
+
+                    // boundary check
+                    if (nextRow < 0 || nextRow >= R || nextCol < 0 || nextCol >= C) continue;
+
+                    // visited check
+                    if (waterDiscovered[nextRow][nextCol]) continue;
+
+                    // cave, stone check
+                    if (map[nextRow][nextCol][0] == STONE || map[nextRow][nextCol][0] == CAVE) continue;
+
+                    map[nextRow][nextCol][step] = WATER;
+                    waterDiscovered[nextRow][nextCol] = true;
+
+                    waterQueue.add(new Point(nextRow, nextCol));
+                }
+            }
+        }
+
+        // 3. hedgehog
+        Queue<Point> hogQueue = new LinkedList<Point>();
+
+        hogQueue.add(hogPos);
+        hogDiscovered[hogPos.row][hogPos.col] = true;
+
+        step = 0;
+        boolean exit = false;
+        while (!hogQueue.isEmpty() && !exit) {
+
+            step++;
+
+            int size = hogQueue.size();
+            for (int i = 0; i < size && !exit; i++) {
+
+                Point u = hogQueue.poll();
+
+                // 상, 하, 좌, 우
+                for (int j = 0; j < 4; j++) {
+
+                    int nextRow = u.row + dRow[j];
+                    int nextCol = u.col + dCol[j];
+
+                    // boundary check
+                    if (nextRow < 0 || nextRow >= R || nextCol < 0 || nextCol >= C) continue;
+
+                    // visited check
+                    if (hogDiscovered[nextRow][nextCol]) continue;
+
+                    // stone check
+                    if (map[nextRow][nextCol][0] == STONE) continue;
+
+                    // water check
+                    if (map[nextRow][nextCol][step] == WATER) continue;
+
+                    // cave check
+                    if (map[nextRow][nextCol][0] == CAVE) {
+
+                        // 4. output
+                        exit = true;
+                        System.out.println(step);
+                        break;
+                    }
+
+                    hogDiscovered[nextRow][nextCol] = true;
+
+                    hogQueue.add(new Point(nextRow, nextCol));
+                }
+
+            }
+        }
+
+        // 4. output
+        if (!exit) {
+
+            System.out.println("KAKTUS");
+        }
+    }
+    
+   static class Point {
+
+    int row;
+    int col;
+
+    Point(int row, int col) {
+
+        this.row = row;
+        this.col = col;
+    }
+}
+
+}
+
 
