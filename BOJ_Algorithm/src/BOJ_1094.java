15d55fc4a5885a042929c100d1cad9ca748ecd8e
Youngyul <31728373+Youngyul@users.noreply.github.com>
2019-02-13 15:45:24 +0900

    Update BOJ_1094.java

--- BOJ_Algorithm/src/BOJ_1094.java
+++ BOJ_Algorithm/src/BOJ_1094.java
@@ -14,11 +14,20 @@ public class BOJ_1094 {
 		
 		for(int i = 1; i<stick.length;i++) {
 			stick[i]= stick[i-1]/2;
-		
 		}
 		
-		//64 , 32, 16, 8 , 4 2 1 
+		int sum = 0;
 		
+		while(x>0) {
+		for(int i =0;i<stick.length;i++) {
+			if(x>=stick[i]) {
+			x= x-stick[i];
+			sum++;
+			}	
+		}
+		}
+		//64 , 32, 16, 8 , 4 2 1 
+		System.out.println(sum);
 	}
 
 }
