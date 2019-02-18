1ecd9e87b0a637cc56325ad0a9b97a256499f7c0
Youngyul <31728373+Youngyul@users.noreply.github.com>
2019-02-18 14:02:58 +0900

    Create BOJ_10844.java

---
+++ BOJ_Algorithm/src/BOJ_10844.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class BOJ_10844 {
+
+	static int result ;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		//N자리 자연수.
+		
+		long [][] dp = new long[101][11];
+		
+		for(int i =1;i<=9;i++) {
+			dp[1][i]=1;
+		}
+		
+		for(int i =2;i<=n;i++) {
+			dp[i][0]=dp[i-1][1];
+			for(int j =1;j<=9;j++) {
+				dp[i][j]= (dp[i-1][j-1]+dp[i-1][j+1])%1000000000;
+				
+			}
+		}
+		
+		long sum = 0;
+		for(int i =0;i<10;i++) {
+			sum+=dp[n][i];
+		}
+		System.out.println(sum%1000000000);
+	}
+
+}
